5d8dfa2cc1d297b25fb96f78f78c992cb678beb2
davist <github@timd.me.uk>
2015-01-20 01:13:28 +0000

    Renamed DHT11 test to EnvironMon as main sensor sketch and added room node

--- arduino/JeeNodeEnvironMon/JeeNodeEnvironMon.ino
+++ arduino/JeeNodeEnvironMon/JeeNodeEnvironMon.ino
@@ -11,9 +11,11 @@
 #define GROUP_ID 212
 #define NODE_ID 4
 
+// dht11 temp/humidity
 #define DHT11_PIN PORT_IRQ_AS_DIO
 #define DHT11_INTERRUPT PORT_IRQ
 
+// Dallas DS18B switchable parasitic power board
 #if defined(__AVR_ATtiny84__) // JNmicro
   #define DS18B_DATA_PIN PORT1_DIO
   #define DS18B_POWER_PIN PORT1_AIO_AS_DIO
@@ -21,6 +23,11 @@
   #define DS18B_DATA_PIN PORT4_DIO
   #define DS18B_POWER_PIN PORT4_AIO_AS_DIO
 #endif
+
+// room node module
+#define HYT131_PORT 2   // defined if HYT131 is connected to a port
+#define LDR_PORT    3   // defined if LDR is connected to a port's AIO pin
+//#define PIR_PORT    3   // defined if PIR is connected to a port's DIO pin
 
 #define SECS_BETWEEN_TRANSMITS 60
 #define SECS_BETWEEN_TRANSMITS_NO_ACK 600
@@ -70,6 +77,67 @@ void dht11_wrapper() {
 
 
 #endif
+
+#if HYT131_PORT
+    PortI2C hyti2cport (HYT131_PORT);
+    HYT131 hyt131 (hyti2cport);
+#endif
+
+#if LDR_PORT
+    Port ldr (LDR_PORT);
+#endif
+
+#if PIR_PORT
+    #define PIR_HOLD_TIME   30  // hold PIR value this many seconds after change
+    #define PIR_PULLUP      1   // set to one to pull-up the PIR input pin
+    #define PIR_INVERTED    1   // 0 or 1, to match PIR reporting high or low
+    
+    /// Interface to a Passive Infrared motion sensor.
+    class PIR : public Port {
+        volatile byte value, changed;
+        volatile uint32_t lastOn;
+    public:
+        PIR (byte portnum)
+            : Port (portnum), value (0), changed (0), lastOn (0) {}
+
+        // this code is called from the pin-change interrupt handler
+        void poll() {
+            // see http://talk.jeelabs.net/topic/811#post-4734 for PIR_INVERTED
+            byte pin = digiRead() ^ PIR_INVERTED;
+            // if the pin just went on, then set the changed flag to report it
+            if (pin) {
+                if (!state())
+                    changed = 1;
+                lastOn = millis();
+            }
+            value = pin;
+        }
+
+        // state is true if curr value is still on or if it was on recently
+        byte state() const {
+            byte f = value;
+            if (lastOn > 0)
+                ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+                    if (millis() - lastOn < 1000 * PIR_HOLD_TIME)
+                        f = 1;
+                }
+            return f;
+        }
+
+        // return true if there is new motion to report
+        byte triggered() {
+            byte f = changed;
+            changed = 0;
+            return f;
+        }
+    };
+
+    PIR pir (PIR_PORT);
+
+    // the PIR signal comes in via a pin-change interrupt
+    ISR(PCINT2_vect) { pir.poll(); }
+#endif
+
 
 HeatHackData dataPacket;
 
@@ -184,6 +252,7 @@ void doMeasure() {
     dataPacket.addReading(SENSOR_LOWBATT, HHSensorType::LOW_BATT, 0);
   }
 
+#if DHT11_PIN
   dht11.acquire();
 
   while (dht11.acquiring()) {
@@ -196,6 +265,7 @@ void doMeasure() {
     dataPacket.addReading(SENSOR_TEMP, HHSensorType::TEMPERATURE, dht11.getCelsius() * 10);
     dataPacket.addReading(SENSOR_HUMIDITY, HHSensorType::HUMIDITY, dht11.getHumidity() * 10);
   }
+#endif
 
 #if DS18B_DATA_PIN
 
@@ -214,6 +284,29 @@ void doMeasure() {
     disableDS18B();
   }
 #endif
+
+#if HYT131_PORT
+    int humi, temp;
+    hyt131.reading(temp, humi);
+
+    payload.addReading(SENSOR_TEMP, HHSensorType::TEMPERATURE, temp);
+    payload.addReading(SENSOR_HUMIDITY, HHSensorType::HUMIDITY, humi);
+#endif
+
+#if LDR_PORT
+    ldr.digiWrite2(1);  // enable AIO pull-up
+    byte light = ~ ldr.anaRead() >> 2;
+    ldr.digiWrite2(0);  // disable pull-up to reduce current draw
+    
+    payload.addReading(SENSOR_LIGHT, HHSensorType::LIGHT, light*10);
+#endif
+
+#if PIR_PORT
+    if (pir.triggered()) {
+      payload.addReading(SENSOR_MOTION, HHSensorType::MOTION, 10);
+    }
+#endif
+
 }
 
 
@@ -249,6 +342,16 @@ void setup() {
   bitSet(DDRB, 0);
   bitClear(PORTB, 0);
 #endif
+
+  #if PIR_PORT
+    pir.digiWrite(PIR_PULLUP);
+    #ifdef PCMSK2
+      bitSet(PCMSK2, PIR_PORT + 3);
+      bitSet(PCICR, PCIE2);
+    #else
+      //XXX TINY!
+    #endif
+  #endif
 
   #if DS18B_DATA_PIN
   pinMode(DS18B_POWER_PIN, OUTPUT);
