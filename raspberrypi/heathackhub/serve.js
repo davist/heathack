0fe58a4455afb80ad6cb93bc4dd78fb57d2975ac
Tim Davis <github@timd.me.uk>
2021-02-25 02:42:40 +0000

    update serialport to latest

--- raspberrypi/heathackhub/serve.js
+++ raspberrypi/heathackhub/serve.js
@@ -1,4 +1,5 @@
-const serialport = require("serialport");
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
 const express = require("express");
 const config = require("./config");
 
@@ -7,14 +8,6 @@ const publishAPI = require('./' + config.publisher);
 
 const publisher = new publishAPI.Publisher(config.publish_settings);
 
-
-const SP = serialport.SerialPort;
-
-// JeeLink on USB0 at 9600 baud default
-const serial = new SP(config.serialport || "/dev/ttyUSB0", {
-	baudrate: config.baudrate || 9600,
-	parser: serialport.parsers.readline("\r\n")
-});
 
 ///////////////////////////
 // data structure to hold readings which the web server publishes
@@ -51,81 +44,80 @@ app.listen(80);
 ///////////////////////////
 // serial listener
 
+// JeeLink on USB0 at 9600 baud default
+const serial = new SerialPort(config.serialport || "/dev/ttyUSB0", {baudRate: config.baudrate || 9600});
+
+const parser = serial.pipe(new Readline());
+
 serial.on("error", function(e) {
 	console.log("Serial port: " + e);
 	process.exit(1);
 });
 
-serial.on("open", function() {
+// event handler for an input string from the serial port
+parser.on("data", function(data) {
 
-	console.log("serial port open");
+	// split string into tokens
+	const tokens = data.toString().split(" ");
 
-	// event handler for an input string from the serial port
-	serial.on("data", function(data) {
+	// if first token (word) isn't "heathack", it's not valid data
+	if (tokens[0] !== "heathack") return;
 
-		// split string into tokens
-		const tokens = data.toString().split(" ");
+	// next token is node id
+	const nodeid = tokens[1];
 
-		// if first token (word) isn't "heathack", it's not valid data
-		if (tokens[0] !== "heathack") return;
+	// get the relevant node object
+	let node = nodeData.nodes[nodeid];
 
-		// next token is node id
-		const nodeid = tokens[1];
+	// if it doesn't exist, create a new one
+	if (!node) {
+		node = {};
+		node.id = nodeid;
+		node.sensors = {};
+		nodeData.nodes[nodeid] = node;
+	}
 
-		// get the relevant node object
-		const node = nodeData.nodes[nodeid];
+	node.lastReadingTime = Date.now();
+
+	// rest of tokens are tuples of sensor id, type and value
+
+	// data to publish to emoncms
+	const curReadings = [];
+
+	for (let i=2; i<tokens.length - 2; i+=3) {
+
+		const sensorid = tokens[i];
+		const type = tokens[i+1];
+		const value = tokens[i+2];
+
+		curReadings.push( {id: sensorid, type: type, value: value} );
+
+		// find sensor object on node
+		let sensor = node.sensors[sensorid];
 
 		// if it doesn't exist, create a new one
-		if (!node) {
-			node = {};
-			node.id = nodeid;
-			node.sensors = {};
-			nodeData.nodes[nodeid] = node;
+		if (!sensor) {
+			sensor = {};
+			sensor.id = sensorid;
+			sensor.type = type;
+			sensor.lastReading = -1;
+			sensor.readings = [];
+			node.sensors[sensorid] = sensor;
 		}
 
-		node.lastReadingTime = Date.now();
+		// update pointer for next reading
+		sensor.lastReading++;
+		if (sensor.lastReading >= nodeData.maxReadings) sensor.lastReading = 0;
 
-		// rest of tokens are tuples of sensor id, type and value
+		// store nodes.maxReadings of readings
+		sensor.readings[sensor.lastReading] = value;
+	}
 
-		// data to publish to emoncms
-		const curReadings = [];
+	if (config.verbose) {
+		console.log("Node " + nodeid + ":");
+		console.log(curReadings);
+	}
 
-		for (let i=2; i<tokens.length - 2; i+=3) {
-
-			const sensorid = tokens[i];
-			const type = tokens[i+1];
-			const value = tokens[i+2];
-
-			curReadings.push( {id: sensorid, type: type, value: value} );
-
-			// find sensor object on node
-			let sensor = node.sensors[sensorid];
-
-			// if it doesn't exist, create a new one
-			if (!sensor) {
-				sensor = {};
-				sensor.id = sensorid;
-				sensor.type = type;
-				sensor.lastReading = -1;
-				sensor.readings = [];
-				node.sensors[sensorid] = sensor;
-			}
-
-			// update pointer for next reading
-			sensor.lastReading++;
-			if (sensor.lastReading >= nodeData.maxReadings) sensor.lastReading = 0;
-
-			// store nodes.maxReadings of readings
-			sensor.readings[sensor.lastReading] = value;
-		}
-
-		if (config.verbose) {
-			console.log("Node " + nodeid + ":");
-			console.log(curReadings);
-		}
-
-		// publish to EmonCMS
-		publisher.publish(nodeid, curReadings);
-	});
-
+	// publish to EmonCMS
+	publisher.publish(nodeid, curReadings);
 });
